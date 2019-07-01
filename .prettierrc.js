cee23935177eca361193886c5b1e47490e4d29a6
ddidwyll <ddidwyll@gmail.com>
2019-07-01 10:06:14 +0400

    Initial

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'none',
+  printWidth: 80,
+  quoteProps: 'consistent',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true
+}
