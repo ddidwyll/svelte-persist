3d948d366c1f11071266cd80400541fa9e6e3f24
ddidwyll <ddidwyll@gmail.com>
2019-07-01 11:22:46 +0400

    Initial

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import { writable } from 'svelte/store'
 
+export default (name, value) => {
+  /* global localStorage */
+  const stored = localStorage.getItem(name)
+  value = writable(!stored ? value : JSON.parse(stored))
+  value.subscribe(value =>
+    value !== undefined ? localStorage.setItem(name, JSON.stringify(value)) : ''
+  )
+  return value
+}
